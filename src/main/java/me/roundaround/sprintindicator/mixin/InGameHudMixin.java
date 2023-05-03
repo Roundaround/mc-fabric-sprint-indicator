00b3f60e92b465ee23ee6e0c726b8ad6da37b428
Evan Steinkerchner <esteinkerchner@gmail.com>
2023-05-03 13:40:25 -0400

    Update Gradle, update to 1.19.4, migrate to kotlin script & RoundaLib plugin

--- src/main/java/me/roundaround/sprintindicator/mixin/InGameHudMixin.java
+++ src/main/java/me/roundaround/sprintindicator/mixin/InGameHudMixin.java
@@ -1,18 +1,7 @@
 package me.roundaround.sprintindicator.mixin;
 
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyArgs;
-import org.spongepowered.asm.mixin.injection.Slice;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
-
 import com.mojang.blaze3d.systems.RenderSystem;
-
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.render.GameRenderer;
@@ -22,9 +11,19 @@ import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.Arm;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.Slice;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 @Mixin(InGameHud.class)
 public abstract class InGameHudMixin {
+  @Final
   @Shadow
   private MinecraftClient client;
   @Shadow
@@ -37,24 +36,21 @@ public abstract class InGameHudMixin {
     return null;
   }
 
-  @Shadow
-  public abstract TextRenderer getTextRenderer();
-
   @Inject(method = "renderHotbar", at = @At(value = "TAIL"))
   private void afterRenderHotbar(float partialTicks, MatrixStack matrixStack, CallbackInfo info) {
     PlayerEntity basePlayer = getCameraPlayer();
-    if (!(basePlayer instanceof ClientPlayerEntity)) {
+    if (!(basePlayer instanceof ClientPlayerEntity player)) {
       return;
     }
 
-    ClientPlayerEntity player = (ClientPlayerEntity) basePlayer;
     if (!player.isSprinting()) {
       return;
     }
 
     Arm arm = player.getMainArm().getOpposite();
 
-    int x = arm == Arm.LEFT ? (this.scaledWidth + 182) / 2 + 6 : (this.scaledWidth - 182) / 2 - 18 - 6;
+    int x =
+        arm == Arm.LEFT ? (this.scaledWidth + 182) / 2 + 6 : (this.scaledWidth - 182) / 2 - 18 - 6;
     int y = this.scaledHeight - 20;
 
     RenderSystem.setShaderColor(1f, 1f, 1f, 1f);
@@ -62,10 +58,11 @@ public abstract class InGameHudMixin {
     RenderSystem.enableBlend();
     RenderSystem.defaultBlendFunc();
 
-    StatusEffectSpriteManager statusEffectSpriteManager = this.client.getStatusEffectSpriteManager();
+    StatusEffectSpriteManager statusEffectSpriteManager =
+        this.client.getStatusEffectSpriteManager();
     Sprite sprite = statusEffectSpriteManager.getSprite(StatusEffects.SPEED);
     RenderSystem.setShaderTexture(0, sprite.getAtlasId());
-    InGameHud.drawSprite(matrixStack, x, y, ((InGameHud) (Object) this).getZOffset(), 18, 18, sprite);
+    InGameHud.drawSprite(matrixStack, x, y, 0, 18, 18, sprite);
   }
 
   // @formatter:off
